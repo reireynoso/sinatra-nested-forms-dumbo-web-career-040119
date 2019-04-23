e57fc7d6ed743581980298c7c0b078e5cf98a5fb
github username <github email address>
2019-04-23 12:12:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,18 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do 
+      @pirate = params[:pirate]
+      erb :"pirates/show"
+    end
+
   end
 end
